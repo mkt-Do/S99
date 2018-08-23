4acd6c8831281c00aba841baa0286c208b6ad6f6
mkt-Do <mkt.koro.mikuru@gmail.com>
2018-08-23 11:46:02 +0900

    P09

--- src/S99.scala
+++ src/S99.scala
@@ -45,5 +45,12 @@ object S99 {
       case x :: xs => e :: x :: xs
     }
   }
+  // P09
+  def pack[A](list: List[A]): List[List[A]] = list.foldRight(Nil: List[List[A]]){
+    (e, ls) => ls match {
+      case (x @ `e` :: _) :: xs => (e :: x) :: xs
+      case _ => List(e) :: ls
+    }
+  }
 }
 
