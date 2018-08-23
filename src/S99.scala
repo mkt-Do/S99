e8c31f3ad9535200bee1cc97d03acc496e416215
mkt-Do <mkt.koro.mikuru@gmail.com>
2018-08-23 11:42:50 +0900

    P02

--- src/S99.scala
+++ src/S99.scala
@@ -5,5 +5,12 @@ object S99 {
     case x :: Nil => x
     case x :: xs => last(xs)
   }
+  // P02
+  def penultimate[A](list: List[A]): A = list match {
+    case Nil => throw new IllegalArgumentException("Nil")
+    case x :: Nil => throw new IllegalArgumentException("Length 1")
+    case x :: y :: Nil => x
+    case x :: y :: ys => penultimate(y :: ys)
+  }
 }
 
